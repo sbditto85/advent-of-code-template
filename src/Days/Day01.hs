0758de244076b6b7fba912873234b6b831942e07
Casey Allred <callred@liveviewtech.com>
2020-12-01 15:58:29 -0700

    day one done, not pretty but whatever

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -1,6 +1,8 @@
 module Days.Day01 (runDay) where
 
 {- ORMOLU_DISABLE -}
+import Control.Monad (join)
+import Debug.Trace
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -21,19 +23,90 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many1' (decimal <* "\n")
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [ Int ]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA (first:numbers) =
+  let
+    trynext first numbers@(next:nexts) =
+      let
+        attempt = trynumber first numbers
+      in
+        if isJust attempt then
+          attempt
+        else
+          trynext next nexts
+
+    trynumber number others =
+      foldr (\next sofar ->
+               if isJust sofar then
+                 sofar
+               else
+                 if next + number == 2020 then
+                   Just (next, number)
+                 else
+                   Nothing
+            ) Nothing others
+  in
+    case trynext first numbers of
+      Nothing -> 0
+      Just (one, two) -> one * two
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB (first:numbers) =
+  let
+    trynext first numbers@(next:nexts) =
+      let
+        attempt = trynumber first numbers
+      in
+        if isJust attempt then
+          attempt
+        else
+          trynext next nexts
+
+    trynumber number others@(first:rest) =
+      let
+        subs :: [ Int ] -> [[ Int ]]
+        subs total@(x:xs) =
+          let
+            numberofrounds = length total - 1
+
+            findSubs (x:xs) round =
+              if round > numberofrounds then
+                []
+              else
+                (sublist x xs) ++ (findSubs (xs ++ [x]) (round + 1))
+          in
+            findSubs total 0
+
+        sublist :: Int -> [ Int ] -> [[ Int ]]
+        sublist x xs =
+          foldr (\next sofar ->
+                   if next + x < 2020 then
+                     [x, next] : sofar
+                   else
+                     sofar
+                ) [] xs
+      in
+        foldr (\(nexta:nextb:[]) sofar ->
+                 if isJust sofar then
+                   sofar
+                 else
+                   if nexta + nextb + number == 2020 then
+                     Just (nexta, nextb, number)
+                   else
+                     Nothing
+              ) Nothing (subs others)
+  in
+    case trynext first numbers of
+      Nothing -> 0
+      Just (one, two, three) -> one * two * three
