071fcbb282102f42d6c6ae8d2b2c1f677d9ddbbb
Casey Allred <callred@liveviewtech.com>
2020-12-05 01:23:33 -0700

    day 5 down, it works

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -1,6 +1,7 @@
 module Days.Day05 (runDay) where
 
 {- ORMOLU_DISABLE -}
+import Debug.Trace (trace)
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -21,19 +22,91 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser =
+  many1 $ ( count 10 (choice [ char 'F' *> pure FrontHalf
+                             , char 'B' *> pure BackHalf
+                             , char 'L' *> pure LeftHalf
+                             , char 'R' *> pure RightHalf
+                             ])
+          ) <* skipSpace
 
 ------------ TYPES ------------
-type Input = Void
 
-type OutputA = Void
+data Indicator
+  = FrontHalf
+  | BackHalf
+  | LeftHalf
+  | RightHalf
+  deriving (Show, Eq)
 
-type OutputB = Void
+type Input = [[Indicator]]
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA seats =
+  maximum $ fmap toUniqueSeatNumber seats
+
+
+toUniqueSeatNumber :: [Indicator] -> Int -- 835
+toUniqueSeatNumber indicators =
+  let
+    rowIndicators = filter (\i -> i == FrontHalf || i == BackHalf) indicators
+    columnIndicators = filter (\i -> i == LeftHalf || i == RightHalf) indicators
+
+    helperRow [] beginning _ = beginning
+    helperRow (i:is) beginning end =
+      if beginning == end then
+        beginning
+      else
+        case i of
+          FrontHalf ->
+            let
+              newEnd = end - (ceiling $ (fromIntegral (end - beginning)) / 2)
+            in
+              helperRow is beginning newEnd
+          BackHalf ->
+            let
+              newBeginning = beginning + ceiling ((fromIntegral (end - beginning)) / 2)
+            in
+              helperRow is newBeginning end
+          _ ->
+            error "shouldn't have anything other than F or B"
+
+    helperColumn [] beginning _ = beginning
+    helperColumn (i:is) beginning end =
+      if beginning == end then
+        beginning
+      else
+        case i of
+          LeftHalf ->
+            let
+              newEnd = end - (ceiling $ (fromIntegral (end - beginning)) / 2)
+            in
+              helperColumn is beginning newEnd
+          RightHalf ->
+            let
+              newBeginning = beginning + ceiling ((fromIntegral (end - beginning)) / 2)
+            in
+              helperColumn is newBeginning end
+          _ ->
+            error "shouldn't have anything other than L or R"
+  in
+    (helperRow rowIndicators 0 127) * 8 + helperColumn columnIndicators 0 7
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> OutputB -- 649
+partB seats =
+  let
+    sortedSeatNums = sort $ fmap toUniqueSeatNumber seats
+    beginning = sortedSeatNums !! 0
+
+    contention = head $ dropWhile (\(i, s) -> i == s) $ zip [beginning..] sortedSeatNums
+  in
+    if fst contention + 1 == snd contention then
+      fst contention
+    else
+      0
