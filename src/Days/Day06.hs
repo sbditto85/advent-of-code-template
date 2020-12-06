336d921d0979dc77f204042e562af4a81d12be0f
Casey Allred <callred@liveviewtech.com>
2020-12-05 22:33:11 -0700

    day 6 done

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -11,8 +11,13 @@ import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
+import qualified Data.Set as S
+import Control.Monad (join)
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
+import qualified Data.Attoparsec.Text as DAT
 import Data.Void
 {- ORMOLU_ENABLE -}
 
@@ -21,19 +26,53 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser =
+  let
+    handleNewGroup = do
+      nextChar <- peekChar'
+      if nextChar == '\n' then
+        fail "done"
+      else
+        skipSpace
+
+    handleNewLine = do
+      nextChar <- peekChar'
+      if nextChar == '\n' then do
+        DAT.take 1
+        pure ()
+      else
+        skipSpace
+
+  in do
+    mls <- many1 $ choice [ fmap Just $ many1 $ handleNewGroup *> (many1 letter) <* handleNewLine
+                          , pure Nothing <* handleNewLine
+                          ]
+    pure $ catMaybes mls
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[[Char]]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> OutputA -- 6878
+partA groups =
+  let
+    sets = fmap (S.fromList . join) groups
+  in
+    sum $ fmap S.size sets
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> OutputB -- 3464
+partB groups =
+  sum $ fmap S.size $ catMaybes $ fmap (\group ->
+                                           foldr (\next sofar ->
+                                                    case sofar of
+                                                      Nothing ->
+                                                        Just $ S.fromList next
+                                                      Just answers ->
+                                                        Just $ answers `S.intersection` (S.fromList next)
+                                                 ) Nothing group
+                                       ) groups
