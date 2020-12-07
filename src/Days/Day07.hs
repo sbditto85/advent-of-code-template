7a97fb674d4f41bae57d6c1cc8c648a61896364c
Casey Allred <callred@liveviewtech.com>
2020-12-07 00:38:08 -0700

    day 7 done

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -11,8 +11,11 @@ import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
+import qualified Data.Attoparsec.Text as DAT
 import Data.Void
 {- ORMOLU_ENABLE -}
 
@@ -21,19 +24,83 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  bags <- many1 $ do
+    bagType <- manyTill anyChar (string " bags contain ")
+    contains <- choice [ many1 $ (,)
+                         <$> (decimal <* skipSpace)
+                         <*> (fmap T.pack $ manyTill anyChar (choice [ string " bag,"
+                                                                     , string " bags,"
+                                                                     , string " bag."
+                                                                     , string " bags."
+                                                                     ]
+                                                             ) <* skipSpace
+                             )
+                       , string "no other bags." *> pure [] <* skipSpace
+                       ]
+    pure $ (T.pack bagType, contains)
+  pure $ Map.fromList bags
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Map Text [ (Int, Text) ]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> OutputA -- 370
+partA bagRules =
+  let
+    myBag = "shiny gold"
+
+    innerToOuter =
+      Map.foldrWithKey (\nextOuter nextInners (sofar :: Map Text [ Text ]) ->
+                          foldr (\(_, nextInner) (sofar' :: Map Text [ Text ]) ->
+                                   Map.alter (\mOuters ->
+                                                case mOuters of
+                                                  Nothing ->
+                                                    Just [nextOuter]
+                                                  Just outers ->
+                                                    Just $ nextOuter : outers
+                                             ) nextInner sofar'
+                                ) sofar nextInners
+                       ) Map.empty bagRules
+
+    helper checkedSofar leftToCheck =
+      if Set.size leftToCheck > 0 then
+        let
+          (toCheck, leftToCheck') = Set.deleteFindMin leftToCheck
+
+          checkedSofar' = Set.insert toCheck checkedSofar
+
+          leftToCheck'' =
+            case Map.lookup toCheck innerToOuter of
+              Nothing ->
+                leftToCheck'
+              Just moreToCheck ->
+                Set.union (Set.fromList moreToCheck) leftToCheck'
+        in
+          helper checkedSofar' leftToCheck''
+      else
+        checkedSofar
+
+  in
+    Set.size $ helper (Set.empty) (Set.fromList $ fromMaybe [] (Map.lookup myBag innerToOuter))
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> OutputB -- 29547
+partB bagRules =
+  let
+    myBag = "shiny gold"
+
+    helper countFor =
+      case Map.lookup countFor bagRules of
+        Nothing ->
+          0
+        Just innerBags ->
+          sum $ fmap (\(numberOf, bagType) ->
+                         numberOf + numberOf * helper bagType
+                     ) innerBags
+  in
+    helper myBag
