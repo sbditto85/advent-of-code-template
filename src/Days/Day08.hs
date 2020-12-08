84b262c2d4df7b0593423401d63d58fea29db878
Casey Allred <callred@liveviewtech.com>
2020-12-07 22:56:09 -0700

    day 8, brute force for the win

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -11,6 +11,7 @@ import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
+import Data.Text (Text)
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
@@ -21,19 +22,86 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser =
+  many1 $ choice [ string "nop" *> (pure Nop <* skipSpace) <*> signed decimal <* skipSpace
+                 , string "acc" *> (pure Acc <* skipSpace) <*> signed decimal <* skipSpace
+                 , string "jmp" *> (pure Jmp <* skipSpace) <*> signed decimal <* skipSpace
+                 ]
 
 ------------ TYPES ------------
-type Input = Void
+data Instructions
+  = Nop Int
+  | Acc Int
+  | Jmp Int
+  deriving (Show)
 
-type OutputA = Void
+type Input = [ Instructions ]
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Either Int Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA instructions = -- 1317
+  let
+    accumuldateUntilRepeat currentInstruction accumulator soFar =
+      let
+        soFar' =
+          Set.insert currentInstruction soFar
+      in
+        if Set.member currentInstruction soFar then
+          accumulator
+        else
+          case instructions !! currentInstruction of
+            Nop _ ->
+              accumuldateUntilRepeat (currentInstruction + 1) accumulator soFar'
+            Acc toInc ->
+              accumuldateUntilRepeat (currentInstruction + 1) (accumulator + toInc) soFar'
+            Jmp relativeJmp ->
+              accumuldateUntilRepeat (currentInstruction + relativeJmp) accumulator soFar'
+  in
+    accumuldateUntilRepeat 0 0 (Set.empty)
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> OutputB -- 1033
+partB instructions =
+  let
+    instructions' = zip [0..] instructions
+    instructionMap = Map.fromList instructions'
+  in
+    foldr (\(idx, instruction) soFar ->
+             case soFar of
+               Right acc ->
+                 soFar
+               Left _ ->
+                 case instruction of
+                   Nop i ->
+                     loopsForever $ fmap snd $ Map.toList $ Map.alter (\_ -> Just $ Jmp i) idx instructionMap
+                   Jmp i ->
+                     loopsForever $ fmap snd $ Map.toList $ Map.alter (\_ -> Just $ Nop i) idx instructionMap
+                   _ ->
+                     Left 0
+          ) (Left 0) instructions'
+
+loopsForever instructions =
+  let
+    accumuldateUntilRepeat currentInstruction accumulator soFar =
+      let
+        soFar' =
+          Set.insert currentInstruction soFar
+      in
+        if length instructions == currentInstruction then
+          Right accumulator
+        else if Set.member currentInstruction soFar then
+          Left accumulator
+        else
+          case instructions !! currentInstruction of
+            Nop _ ->
+              accumuldateUntilRepeat (currentInstruction + 1) accumulator soFar'
+            Acc toInc ->
+              accumuldateUntilRepeat (currentInstruction + 1) (accumulator + toInc) soFar'
+            Jmp relativeJmp ->
+              accumuldateUntilRepeat (currentInstruction + relativeJmp) accumulator soFar'
+  in
+    accumuldateUntilRepeat 0 0 (Set.empty)
